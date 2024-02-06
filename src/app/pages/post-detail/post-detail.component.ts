f0951309b9acec4e5a48293b17a7001015c75186
khoa1406 <khoa.nd11530@sinhvien.hoasen.edu.vn>
2024-02-07 00:10:21 +0700

    update

--- src/app/pages/post-detail/post-detail.component.ts
+++ src/app/pages/post-detail/post-detail.component.ts
@@ -1,9 +1,10 @@
+import { ReplyComponent } from './../reply/reply.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [],
+  imports: [ReplyComponent],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.scss'
 })
